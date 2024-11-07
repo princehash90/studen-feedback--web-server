4793b24462553b3408a9238256f01e6431d1fdc7
princehash90 <hashim.muridi@go.minneapolis.edu>
2024-11-07 11:15:29 -0600

    initial committ

---
+++ wk10/BodyMassIndexForm/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import BodyMassIndexForm from './components/BodyMassIndexForm.vue'
+import  {ref} from 'vue'
+
+const totalBmi= ref('')
+
+function calculateBMI(height, weight){// function to calculate to bmi
+  const bmi = weight / (height * height)
+  totalBmi.value = `The BMI is ${bmi.toFixed(2)}`
+}
+
+</script>
+
+<template>
+  <div id="app-compo">
+      <h1>Body Mass Index Calculator</h1>
+
+      <BodyMassIndexForm v-on:stats-entered="calculateBMI">
+      </BodyMassIndexForm>
+
+
+ <p>{{totalBmi}}</p>
+
+  </div>
+
+
+</template>
+
+<style scoped>
+#app-compo{
+  border: 3px solid blue;
+}
+
+</style>
---
+++ wk10/BodyMassIndexForm/src/components/BodyMassIndexForm.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref, computed} from 'vue'
+
+const heightInMeters= ref('')// reactive data of the height
+const weightInKg= ref('')//reactive data of the weight
+
+const emit =defineEmits([// emits the data user entered to the parent
+    'stats-entered'
+])
+
+function userInput(){
+  emit('stats-entered', heightInMeters.value,weightInKg.value)
+}// this is the height and weight to be emitted to the parent, App.vue
+
+</script>
+
+<template>
+
+  <div id="user-inputs">
+    <h2>Enter your height and weight</h2>
+      <label for="meters">Height in Meters</label>
+      <input type="number" v-model="heightInMeters" id="meters" ><br><!--using v-model to connect to the user input-->
+      <label for="weight">Weight in Kilogram</label>
+      <input type="number" v-model="weightInKg" id="weight" ><br>
+      <button id="calculate" v-on:click="userInput">Calculate</button>
+  </div>
+
+
+</template>
+
+<style scoped>
+
+#user-inputs{
+  border: 3px solid red;
+}
+#calculate{
+  background-color: #646cff;
+}
+</style>
